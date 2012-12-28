68454d95cbdfd91cebd17edcf04a0ed88ae3ef42
Ronaldo Nascimento <ronaldo1@users.sf.net>
2012-12-28 17:07:14 -0500

    implementing markdown, ruby version
    thanks to raibow

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-battle
-======
+# battle #
 
 A battle simulator.
---
+++ ruby/battle.rb
@@ -0,0 +1,2 @@
+require 'rainbow'
+puts "this is red".foreground(:red) + " and " + "this on yellow bg".background(:yellow) + " and " + "even bright underlined!".underline.bright
