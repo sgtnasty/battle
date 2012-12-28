4cddb8f1a17c428dbd6eb03efc5281009d52458b
Ronaldo Nascimento <ronaldo1@users.sf.net>
2012-12-28 17:22:46 -0500

    rainbow info

--- ruby/README.md
+++ ruby/README.md
@@ -1,4 +1,9 @@
 In order to get this script to work you must invoke with:
     ruby -rubygems battle.rb
 
+Nah, i think i fixed that, but you do need to install 'rainbow':
+~~~
+     gem install rainbow
+~~~
+
 -thanks
--- ruby/battle.rb
+++ ruby/battle.rb
@@ -1,3 +1,3 @@
-#!/usr/bin/ruby
+#!/usr/bin/ruby -rubygems
 require 'rainbow'
 puts "this is red".foreground(:red) + " and " + "this on yellow bg".background(:yellow) + " and " + "even bright underlined!".underline.bright
