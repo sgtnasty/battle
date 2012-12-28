62e095f05a19720f22d08dcaaf3eff07c0edaa07
Ronaldo Nascimento <ronaldo1@users.sf.net>
2012-12-28 17:16:40 -0500

    ruby doesnt like my require
    put invocation info in the readme

---
+++ ruby/README.md
@@ -0,0 +1,4 @@
+In order to get this script to work you must invoke with:
+    ruby -rubygems battle.rb
+
+-thanks
--- ruby/battle.rb
+++ ruby/battle.rb
@@ -1,2 +1,3 @@
+#!/usr/bin/ruby
 require 'rainbow'
 puts "this is red".foreground(:red) + " and " + "this on yellow bg".background(:yellow) + " and " + "even bright underlined!".underline.bright
