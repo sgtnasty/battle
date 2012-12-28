2479f2054497bd064d6506a711fe6f4df3e2328a
Ronaldo Nascimento <ronaldo1@users.sf.net>
2012-12-28 17:30:02 -0500

    added GPL3

--- ruby/battle.rb
+++ ruby/battle.rb
@@ -1,3 +1,22 @@
 #!/usr/bin/ruby -rubygems
+# battle Copyright (c) 2012 by Ronaldo Nascimento
+# This file is part of battle.
+# 
+# battle is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# battle is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with battle.  If not, see <http://www.gnu.org/licenses/>.
+
 require 'rainbow'
+
+
+
 puts "this is red".foreground(:red) + " and " + "this on yellow bg".background(:yellow) + " and " + "even bright underlined!".underline.bright
