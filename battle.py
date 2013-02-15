b66a85b5810f48f1c1b3a0fbf1f2033cda7cbaf3
Ronaldo Nascimento <ronaldo.nascimento@va.gov>
2013-02-15 14:56:58 -0500

    working on careers

--- battle.py
+++ battle.py
@@ -19,9 +19,17 @@
 """
 
 from gi.repository import Gtk, Gdk
-import random, string, math
+from os.path import abspath, dirname, join
+import random, string, math, cairo
+
+RESOURCES = abspath(dirname(__file__))
 
 class Handler():
+	
+	def __init__(self):
+		# Create buffer
+		self.double_buffer = None
+	
 	def on_window1_delete_event(self, *args):
 		Gtk.main_quit(*args)
 	
@@ -38,12 +46,70 @@ class Handler():
 		ad.set_license('Distributed under the GPL v3 license.\nhttp://www.gnu.org/licenses/gpl-3.0.html')
 		ad.run()
 		ad.destroy()
+		
+	def on_drawingarea1_configure_event(self, widget, event, data=None):
+		"""Configure the double buffer based on size of the widget"""
+		print("on_drawingarea1_configure_event")
+		# Destroy previous buffer
+		if self.double_buffer is not None:
+			self.double_buffer.finish()
+			self.double_buffer = None
+		# Create a new buffer
+		self.double_buffer = cairo.ImageSurface(\
+			cairo.FORMAT_ARGB32,
+			widget.get_allocated_width(),
+			widget.get_allocated_height()
+			)
+		# Initialize the buffer
+		self.draw_something()
+		return False
+		
+	def on_drawingarea1_draw(self, widget, cr):
+		"""Throw double buffer into widget drawable"""
+		print("on_drawingarea1_draw")
+		if self.double_buffer is not None:
+			cr.set_source_surface(self.double_buffer, 0.0, 0.0)
+			cr.paint()
+		else:
+			print('Invalid double buffer')
+		return False
+		
+	def draw_something(self):
+		"""Draw something into the buffer"""
+		print("draw_something")
+		db = self.double_buffer
+		if db is not None:
+			print(db)
+			# Create cairo context with double buffer as is DESTINATION
+			cc = cairo.Context(db)
+			# Scale to device coordenates
+			cc.scale(db.get_width(), db.get_height())
+			# Draw a white background
+			cc.set_source_rgb(1, 1, 1)
+			# Draw something, in this case a matrix
+			rows = 10
+			columns = 10
+			cell_size = 1.0 / rows
+			line_width = 1.0
+			line_width, notused = cc.device_to_user(line_width, 0.0)
+			for i in range(rows):
+				for j in range(columns):
+					cc.rectangle(j * cell_size, i * cell_size, cell_size, cell_size)
+					cc.set_line_width(line_width)
+					cc.set_source_rgb(0, 0, 0)
+					cc.stroke()
+			# Flush drawing actions
+			db.flush()
+		else:
+			print('Invalid double buffer')
+
 
 def battle():
 	builder = Gtk.Builder()
-	builder.add_from_file("battle.glade")
+	builder.add_from_file(join(RESOURCES,"battle.glade"))
 	builder.connect_signals(Handler())
 	window = builder.get_object("window1")
+	drawingarea1 = builder.get_object("drawingarea1")
 	window.show_all()
 	Gtk.main()
 
